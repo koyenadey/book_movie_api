17d137ce3cb4a69a411dcd9fea5b7e5aff8bca15
koyena.dey <koyenadey8@gmail.com>
2024-12-06 14:00:19 +0200

    Added the api routes for cast controller

--- src/cast-crew/cast-crew.controller.ts
+++ src/cast-crew/cast-crew.controller.ts
@@ -14,36 +14,53 @@ import { CastCrewService } from './cast-crew.service';
 import { CreateCastCrewDto } from './dto/create-cast-crew.dto';
 import { UpdateCastCrewDto } from './dto/update-cast-crew.dto';
 import { OptionalUUIDPipe } from 'src/pipes/optionalUuidPipe.pipe';
+import { ApiBody, ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { ResponseCastDto } from './dto/response-cast-crew.dto';
 
 @Controller('casts')
 export class CastCrewController {
   constructor(private readonly castCrewService: CastCrewService) {}
 
   @Post()
-  createCastCrew(@Body(ValidationPipe) createCastCrewDto: CreateCastCrewDto) {
+  @ApiBody({ type: CreateCastCrewDto })
+  @ApiCreatedResponse({ type: ResponseCastDto })
+  createCastCrew(
+    @Body(ValidationPipe) createCastCrewDto: CreateCastCrewDto,
+  ): Promise<ResponseCastDto> {
     return this.castCrewService.createCastCrew(createCastCrewDto);
   }
 
   @Get()
-  getAllCastCrew(@Query('movieId', OptionalUUIDPipe) movieId?: string) {
+  @ApiOkResponse({ type: ResponseCastDto })
+  getAllCastCrew(
+    @Query('movieId', OptionalUUIDPipe) movieId?: string,
+  ): Promise<ResponseCastDto[]> {
     return this.castCrewService.getAllCastCrew(movieId);
   }
 
   @Get(':id')
-  getCastCrewById(@Param('id', ParseUUIDPipe) id: string) {
+  @ApiOkResponse({ type: ResponseCastDto })
+  getCastCrewById(
+    @Param('id', ParseUUIDPipe) id: string,
+  ): Promise<ResponseCastDto> {
     return this.castCrewService.getCastCrewById(id);
   }
 
   @Patch(':id')
+  @ApiBody({ type: UpdateCastCrewDto })
+  @ApiOkResponse({ type: ResponseCastDto })
   updateCastCrewById(
     @Param('id', ParseUUIDPipe) id: string,
     @Body(ValidationPipe) updateCastCrewDto: UpdateCastCrewDto,
-  ) {
+  ): Promise<ResponseCastDto> {
     return this.castCrewService.updateCastCrewById(id, updateCastCrewDto);
   }
 
   @Delete(':id')
-  deleteCastCrewById(@Param('id', ParseUUIDPipe) id: string) {
+  @ApiOkResponse({ type: ResponseCastDto })
+  deleteCastCrewById(
+    @Param('id', ParseUUIDPipe) id: string,
+  ): Promise<ResponseCastDto> {
     return this.castCrewService.deleteCastCrewById(id);
   }
 }
