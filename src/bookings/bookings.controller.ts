ff59fd00b14913168674df7ef208c759b3466a66
koyena.dey <koyenadey8@gmail.com>
2024-12-11 23:34:22 +0200

    Added the guards in the bookings module

--- src/bookings/bookings.controller.ts
+++ src/bookings/bookings.controller.ts
@@ -9,6 +9,7 @@ import {
   ValidationPipe,
   Query,
   ParseUUIDPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
 import { CreateBookingDto } from './dto/create-booking.dto';
@@ -16,6 +17,10 @@ import { UpdateBookingDto } from './dto/update-booking.dto';
 import { OptionalUUIDPipe } from 'src/pipes/optionalUuidPipe.pipe';
 import { ApiBody, ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 import { ResponseBookingDto } from './dto/response-booking.dto';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { MemberRoles } from '@prisma/client';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 @Controller('bookings')
 export class BookingsController {
@@ -29,36 +34,47 @@ export class BookingsController {
   }
 
   @Get()
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(MemberRoles.Admin)
   @ApiOkResponse({ type: ResponseBookingDto })
   getAllBookings() {
     return this.bookingsService.getAllBookings();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   @ApiOkResponse({ type: ResponseBookingDto })
   getBookingDetailsById(@Param('id', ParseUUIDPipe) id: string) {
     return this.bookingsService.getBookingDetailsById(id);
   }
 
   @Get(':movieId')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(MemberRoles.Admin)
   @ApiOkResponse({ type: ResponseBookingDto })
   getBookingsByMovieId(@Param('movieId', ParseUUIDPipe) movieId: string) {
     return this.bookingsService.getBookingsByMovieId(movieId);
   }
 
   @Get(':memberId')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(MemberRoles.Admin)
   @ApiOkResponse({ type: ResponseBookingDto })
   getBookingsByMemberId(@Param('memberId', ParseUUIDPipe) memberId: string) {
     this.bookingsService.getBookingsByMemberId(memberId);
   }
 
   @Get(':theatreId')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(MemberRoles.Admin)
   @ApiOkResponse({ type: ResponseBookingDto })
   getBookingsByTheatreId(@Param('theatreId', ParseUUIDPipe) theatreId: string) {
     this.bookingsService.getBookingsByTheatreId(theatreId);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(MemberRoles.Admin)
   @ApiBody({ type: UpdateBookingDto })
   @ApiOkResponse({ type: ResponseBookingDto })
   updateByBookingId(
@@ -69,6 +85,8 @@ export class BookingsController {
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(MemberRoles.Admin)
   @ApiOkResponse({ type: ResponseBookingDto })
   deleteBookingById(@Param('id') id: string) {
     return this.bookingsService.deleteBookingById(id);
